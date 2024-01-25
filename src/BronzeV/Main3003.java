25315d82c80f9568b312c1e20f182c0b213ffa3e
dbehdrbs0806 <a67422830@gmail.com>
2024-01-25 17:32:56 +0900

    4101

--- src/BronzeV/Main3003.java
+++ src/BronzeV/Main3003.java
@@ -1,5 +1,32 @@
 package BronzeV;
 
-public class Main3003 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Main3003 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        String[] white = buffer.readLine().split(" ");
+        int[] black = {1, 1, 2, 2, 2, 8};
+
+        System.out.println((black[0]-Integer.parseInt(white[0])) + " " +
+                (black[1]-Integer.parseInt(white[1])) + " " + (black[2]-Integer.parseInt(white[2]))
+        + " " + (black[3]-Integer.parseInt(white[3])) + " " + (black[4]-Integer.parseInt(white[4]))
+        + " " + (black[5]-Integer.parseInt(white[5])));
+    }
 }
+/*입력값을 받을 때 무조건 BufferedReader 를 사용하기
+
+BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 로 객체 사용함
+throws Exception 을 해줘야함
+문자열 입력받기
+String[] input = buffer.readLine();
+정수 입력받기
+int n = Integer.parseInt(buffer.readLine());
+공백으로 구분된 정수 입력받기
+String[] input = buffer.readLine().split(" ");
+int n1 = Integer.parseInt(input[0]);
+
+String 객체는 변경이 불가함으로
+StringBuilder st = new StringBuilder(); 를 사용하는 것이 좋다
+ */
---
+++ src/BronzeV/Main4101.java
@@ -0,0 +1,25 @@
+package BronzeV;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main4101 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String[] input = buffer.readLine().split(" ");
+            String a1 = input[0];
+            String a2 = input[1];
+            if (Integer.parseInt(a1) == 0 && Integer.parseInt(a2) == 0) {
+                break;
+            }
+            else if (Integer.parseInt(a1) > Integer.parseInt(a2)) {
+                System.out.println("Yes");
+            }
+            else if (Integer.parseInt(a1) <= Integer.parseInt(a2)) {
+                System.out.println("No");
+            }
+        }
+    }
+}
