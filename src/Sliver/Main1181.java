1351c1cb62648ca1dc71e12fe12d19f252d9b8a7
dbehdrbs0806 <80169727+dbehdrbs0806@users.noreply.github.com>
2024-07-20 20:30:08 +0900

    2024-07-20 백준 1181번 해결

---
+++ src/Sliver/Main1181.java
@@ -0,0 +1,45 @@
+package Sliver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+import java.lang.reflect.Array;
+
+
+public class Main1181 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<String> array = new ArrayList<>();
+        int input = Integer.parseInt(br.readLine());
+        String token;
+        String temp;
+        for(int i = 0; i < input; i++) {
+            token = br.readLine();
+            array.add(token);
+        }
+        for (int i = input; i > 0; i--) {
+            for (int j = 1; j < i; j++) {
+                if ((array.get(j - 1)).length() > (array.get(j)).length()) {
+                    temp = array.get(j - 1);
+                    array.set(j - 1, array.get(j));
+                    array.set(j, temp);
+                }
+                else if ((array.get(j - 1)).length() == (array.get(j).length())) {
+                    if (array.get(j - 1).compareTo(array.get(j)) > 0) {
+                        temp = array.get(j - 1);
+                        array.set(j - 1, array.get(j));
+                        array.set(j, temp);
+                    }
+                }
+            }
+        }
+        Set<String> result = new LinkedHashSet<>(array);
+
+        for (String k : result) {
+            System.out.println(k);
+        }
+    }
+}
+// but, no, wont, i, hesitate
