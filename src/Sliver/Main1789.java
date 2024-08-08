fae11e6de02e39b296604ffb99a57a944e7cea6a
dbehdrbs0806 <80169727+dbehdrbs0806@users.noreply.github.com>
2024-08-08 18:58:57 +0900

    백준 1789번

---
+++ src/Sliver/Main1789.java
@@ -0,0 +1,21 @@
+package Sliver;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1789 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long input = Long.parseLong(br.readLine());
+        long a = 1, result = 0, count = 0;
+        while (true) {
+            result += a;
+            a++;
+            count++;
+            if (result > input) {
+                break;
+            }
+        }
+        System.out.println(count - 1);
+    }
+}
